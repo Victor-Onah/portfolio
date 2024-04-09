a456b86422dadb9a7550bb1fdc40dc979af5470f
chukwuu <uchenna.chukwu@atrizult.com>
2024-04-09 11:33:11 +0100

    Removed some unwanted projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,13 +3,6 @@ import { useEffect, useId, useRef } from "react";
 const Projects = () => {
 	const projects = [
 		{
-			name: "Mma Beauty Care",
-			description:
-				"Mma Beauty Care is a simple full-stack e-commerce website that deals on cosmetics and allows users to add items to their cart and place orders. It was built with Tailwind CSS, React JS, Express JS, Node JS and Mongo DB",
-			link: "https://mma-beauty-care.netlify.app",
-			imageUrl: "/images/mbc.png",
-		},
-		{
 			name: "Iyke Bulkwill",
 			description:
 				"Iyke-Bulkwill.com is a full-stack e-commerce website which I built for a client (Mr. Iyke). The website allows users to find and purchase materials for interior decoration, security lights, lands, mobile accessories and so on. It was built with Tailwind CSS, Next Js, React JS, Express JS, Node JS and Mongo DB.",
@@ -22,13 +15,6 @@ const Projects = () => {
 				"Naija Quotes is a simple full-stack website. It was a fun project to build because it gave me the chance to practice my Vanilla JS without any frameworks. It was built with Vanilla JS, Tailwind CSS, Express JS, Node JS and Mongo DB.",
 			link: "https://naijaquotes.onrender.com",
 			imageUrl: "/images/naija_quotes.png",
-		},
-		{
-			name: "Cryptic",
-			description:
-				"Cryptic is a simple landing page design for a crypto currency website. It features a deep blue background and white foreground (for text and images). It was built with React.js and Tailwind CSS.",
-			link: "https://cryptic-j0p0.onrender.com/",
-			imageUrl: "/images/cryptic.png",
 		},
 		{
 			name: "React Form",
